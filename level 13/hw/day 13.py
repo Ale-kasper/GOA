86b57b3955334752082a170e6a64891fd4ddb0f3
Ale-kasper <gelovanialexandra@gmail.com>
2025-08-15 18:28:35 +0400

    second commit

--- level 13/hw/day 13.py
+++ level 13/hw/day 13.py
@@ -1,1 +1,83 @@
-print("vaketeb hw")
+#1 nomeri
+# #for loop printavs momxmareblit daweril sityvas ramdenjerac unda.
+
+
+#2 nomeri
+num = int(input("enter your number: "))
+if num > 0:
+  print("dadebiti")
+elif num < 0:
+  print("uaryopiti")
+else:
+  print("tolia")
+
+
+
+
+
+
+
+#3 nomeri
+for number in range (20):
+
+  if number % 2 == 0:
+    print(number ,"luwi")
+  else:
+    print(number ,"kenti")
+
+#4 nomeri
+num = int(input("enter your number: "))
+if num == 100:
+  print("maladec")
+elif num < 50:
+  print("ver chaabare")
+elif num == 50:
+  print("dzlivs gadaxvedi")
+else:
+  print("normaluria")
+
+
+
+
+
+#5 nomeri
+for number in range (10):
+
+  if number % 2 == 0:
+    print(number ,"luwi")
+  else:
+    print(number ,"kenti")
+
+
+
+for number in range (50):
+
+  if number % 2 == 0:
+    print(number ,"luwi")
+  else:
+    print(number ,"kenti")
+
+
+for number in range (5):
+
+  if number % 2 == 0:
+    print(number ,"luwi")
+  else:
+    print(number ,"kenti")
+
+
+
+for number in range (30):
+
+  if number % 2 == 0:
+    print(number ,"luwi")
+  else:
+    print(number ,"kenti")
+
+
+for number in range (15):
+
+  if number % 2 == 0:
+    print(number ,"luwi")
+  else:
+    print(number ,"kenti")
