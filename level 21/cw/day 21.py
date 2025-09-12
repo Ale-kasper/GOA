d33c3060f09f8d22266bf8507a1bbb03b66df10d
Ale-kasper <gelovanialexandra@gmail.com>
2025-09-12 16:54:52 +0400

    second commit

---
+++ level 21/cw/day 21.py
@@ -0,0 +1,10 @@
+array = ["kasperito", 'wow', 'till', 'mizi', 'labewbew', 'mirror', 'butter', 'fly', 'six', 'seven']
+
+print(len(array))
+
+
+for i in range (len(array)):
+    print(i, array)
+
+
+print(array [::-1])
