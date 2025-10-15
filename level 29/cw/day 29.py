d80ca8b3c25407ec70f93c4e10acdb2f9fa7b0cb
Aleksandra Gelovani <gelovanialexandra@gmail.com>
2025-10-15 17:36:46 +0400

    second commit

---
+++ level 29/cw/day 29.py
@@ -0,0 +1,20 @@
+array = [1, 2, 3, 3, 4, 5, 6, 7, 9,9,9,9,9,9,11,11,12,12]
+
+array_index = array.index(9)
+print(array_index)
+
+
+
+
+
+def lion():
+    return "im lion"
+
+print(lion())
+
+
+
+
+def summer_func(a, b):
+    kasper = a + b
+    print(kasper)
