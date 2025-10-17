bf6bbe8cce2bff15e642207b2213b9eadf0f4b06
Aleksandra Gelovani <gelovanialexandra@gmail.com>
2025-10-17 19:03:55 +0400

    second commit

---
+++ level 31/cw/day 31.py
@@ -0,0 +1,12 @@
+# #1) Create function named skeleton give two parameters a and b
+
+# if a + b  should equal 55 return "Here u go 55 bone"
+# otherwise "U never get bones!"
+
+a = 50
+b = 5
+def skeleton(a, b):
+    if a + b == 55:
+      return("here u go 55 bone")
+    else:
+       return("u never get bones!")
